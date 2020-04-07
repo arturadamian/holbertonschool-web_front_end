507528bb20bfc90f1264c273ebff10d50cbe9e9c
arturadamian <arturadamian@gmail.com>
2020-04-06 23:55:40 -0700

    add some code

---
+++ 0x0C-Javascript_advanced/100-stock.js
@@ -0,0 +1,16 @@
+let stock = { macbook: 2, iphone: 4 } 
+let processPayment = (itemName) => {
+    stock[itemName] -= 1;
+    console.log(`Payment is being processed for item ${itemName}`);
+}
+let processError = (itemName) => {
+    console.log(`No more ${itemName} in stock`);
+    console.log(`Payment is not being processed`);
+}
+let processOrder = (itemName, callbackPayment, callbackError) => {
+    console.log(`Verifying the stock of ${itemName}`);
+    (stock[itemName] > 0) ? processPayment(itemName) : processError(itemName);
+}
+let itemName = prompt("Please enter the item you would like to purchase (Macbook, iPhone");
+itemName.toLowerCase
+!(itemName in stock) ? console.log(`Sorry, we dont have ${itemName}`) : processOrder(itemName);
